f801fa7d0d6fecfe65d236f9cd287b492934b871
Yuichi Fujiki <yuichi@chaione.com>
2020-06-07 14:49:40 +0800

    Startup

--- SwiftUIGraph-SB-TechTalk/ContentView.swift
+++ SwiftUIGraph-SB-TechTalk/ContentView.swift
@@ -8,33 +8,9 @@
 
 import SwiftUI
 
-struct MyGraph: Shape {
-    let originalData: Data = Data(startIndex: 0, endIndex: Data.originalDataCount - 1)
-
-    func path(in rect: CGRect) -> Path {
-        let step = rect.width / CGFloat(originalData.endIndex - originalData.startIndex + 1)
-        var path = Path()
-
-        for i in originalData.startIndex ... originalData.endIndex {
-            let index = i - originalData.startIndex
-            let x = CGFloat(index) * step
-            let y = originalData[index]
-            let point = CGPoint(x: x, y: y)
-            if i == originalData.startIndex {
-                path.move(to: point)
-            } else {
-                path.addLine(to: point)
-            }
-        }
-
-        return path
-    }
-}
-
 struct ContentView: View {
     var body: some View {
-        MyGraph()
-            .stroke(Color.red, lineWidth: 4)
+        Text("Hello World")
     }
 }
 
